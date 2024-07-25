081272f3631d00ee7fb2402ae9fb9d6bff2dfaee
Jay Lee <91929282+LeeJaejun98@users.noreply.github.com>
2024-07-25 20:42:38 +0900

    Add files via upload

---
+++ Cookudasse_test/src/main/java/com/example/cookudasse_test/CookudasseTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.cookudasse_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookudasseTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookudasseTestApplication.class, args);
+    }
+
+}
---
+++ Cookudasse_test/src/test/java/com/example/cookudasse_test/CookudasseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cookudasse_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookudasseTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
